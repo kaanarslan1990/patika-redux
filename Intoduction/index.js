cbafdac875e27d75148047b4fded6f2ef07df6ed
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-24 11:38:47 +0300

    patika-redux introdoction and  conterapp

---
+++ Intoduction/index.js
@@ -0,0 +1,30 @@
+// let name = "Mehmet";
+// let name2 = name;
+
+// name2 = "Murayt"
+// console.log(name);
+// console.log(name2)
+
+// const user = {
+//   name: "Mehmet",
+//   isActive: true,
+// };
+
+// const user2 = Object.assign({}, user);
+// const user2 = {...user};
+// const user2 = user;
+
+// user2.name = "murat";
+// user2.isActive= false;
+
+// console.log(user);
+// console.log(user2);
+
+const numbers = [1,2,3,4]
+const numbers2 = [...numbers]
+
+numbers2.push(5)
+
+console.log(numbers)
+console.log(numbers2)
+
