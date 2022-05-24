8487cf342b7fb027f5c6308f8fc22245c8378d14
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-24 12:29:20 +0300

    get state value withth useSelector

---
+++ rdx-counterapp/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function Counter() {
+
+  const countValue = useSelector((state) => state);
+  
+  console.log(countValue);
+
+  return <div>{/* <h1>{countValue}</h1> */}</div>;
+}
+
+export default Counter;
