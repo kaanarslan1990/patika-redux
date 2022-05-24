bf38727a7bb061167c3063e32cb3eaea78a5157a
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-24 16:47:04 +0300

    add toggle action

--- rdx-todo-app/src/components/TodoList.js
+++ rdx-todo-app/src/components/TodoList.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../redux/todos/todosSlice";
 function TodoList() {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items);
 
   return (
     <ul className="todo-list">
-      
       {items.map((item) => (
-        <li key= {item.id} className={item.completed ? "completed" : "" }>
+        <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
             <button className="destroy"></button>
           </div>
