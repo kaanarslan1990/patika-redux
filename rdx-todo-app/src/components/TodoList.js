504feb4cb6c45aa667adb60cb9f316f503538050
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-25 11:21:50 +0300

    add selector to slice and react.memo to footer

--- rdx-todo-app/src/components/TodoList.js
+++ rdx-todo-app/src/components/TodoList.js
@@ -1,31 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { destroy, toggle } from "../redux/todos/todosSlice";
+import { destroy, toggle, selectTodosFiltered } from "../redux/todos/todosSlice";
 
-let filtered = [];
+
 
 function TodoList() {
   const dispatch = useDispatch();
-  const items = useSelector((state) => state.todos.items);
-  const activeFilter = useSelector((state) => state.todos.activeFilter);
-
+  const filteredTodos = useSelector(selectTodosFiltered)
+  
   const handleDestroy = (id) => {
     if (window.confirm("Are you sure?")) {
       dispatch(destroy(id));
     }
   };
 
-  filtered = items;
-
-  if (activeFilter !== "all") {
-    filtered = items.filter((todo) =>
-      activeFilter === "active" ? todo.completed === false : todo.completed === true
-    );
-  }
+ 
 
   return (
     <ul className="todo-list">
-      {filtered.map((item) => (
+      {filteredTodos.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
             <input
