69774a6f2d35d9e75d90ca24bd960e78bd67913a
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-01 12:59:21 +0300

    c8<quotes detail page>

--- rdx-breaking-bad/src/App.js
+++ rdx-breaking-bad/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Quotes from './pages/Quotes';
+import QuoteDetail from './pages/QuoteDetail';
 // import CharacterDetail from './components/CharacterDetail';
 // import Quotes from './pages/Quotes';
 // import QuoteDetail from './pages/QuoteDetail';
@@ -23,8 +24,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/char/:char_id" element={<Detail />} />
-        <Route exact path="/quotes" element={<Quotes />} />
-        {/* <Route path="/quotes/:quote_id" element={<QuoteDetail />} /> */}
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="/quotes/:quote_id" element={<QuoteDetail />} />
       </Routes>
     </div>
   );
---
+++ rdx-breaking-bad/src/pages/QuoteDetail/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams,Navigate  } from "react-router-dom";
+import Loading from "../../components/Loading";
+
+
+function QuoteDetail() {
+  const { quote_id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate()
+
+  const quote = useSelector((state) =>
+    state.quotes.items.find((item) => item.quote_id === Number(quote_id))
+  );
+  if (!quote) {
+      return <Navigate to="/quotes" />
+  }
+
+  return (
+    <div>
+      {loading && !<Loading />}
+      {quote && (
+        <div className="quoteContainer">
+          
+          <div className="singleQuote">
+            <h3>Id:</h3>
+            <p>{quote.quote_id}</p>
+            <h3>Quote:</h3>
+            <p>{quote.quote}</p>
+            <h3>Author:</h3>            
+            <p>{quote.author}</p>
+            <h3>Series:</h3>
+            <p>{quote.series}</p>
+          </div>
+        </div>
+      )}
+      <hr />
+      {loading && <button onClick={() => navigate(-1)}>Go back</button>}
+    </div>
+  );
+}
+
+export default QuoteDetail;
